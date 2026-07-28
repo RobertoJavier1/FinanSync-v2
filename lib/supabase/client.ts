411925f9b6d76db3103ac73d9b61465286c161c9
Roberto Molina <24000849@galileo.edu>
2026-07-28 11:48:28 -0600

    conexion con supabase

---
+++ lib/supabase/client.ts
@@ -0,0 +1,7 @@
+//crea el cliente de Supabase para usarlo desde el navegador
+import { createBrowserClient } from '@supabase/ssr'
+
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
